ef576da8d8863736ec06785f3476503aa3a32fd4
euansu <349980554@qq.com>
2023-05-18 12:20:06 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--skip-torch-cuda-test --no-half --use-cpu all --gradio-img2img-tool color-sketch
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --no-half --use-cpu all --gradio-img2img-tool color-sketch --ngrok 2PbEKINEPUGXB5MOSOvFiwaakeH_3TRPa8BBwrPS1Qu3fyhz --gradio-auth a:a
 
 call webui.bat
